24315e2edb4ccb2821341f6a40e92668e3c5b66c
Abdulnaeem123 <64002522+Abdulnaeem123@users.noreply.github.com>
2020-04-21 12:56:47 +0530

    Add files via upload

---
+++ abdul1.java
@@ -0,0 +1,17 @@
+package basicprograms;
+
+public class sum_of_odd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=0;
+		for (int i = 1; i <=100; i++) {
+			if(i%2!=0)
+			{
+				sum=sum+i;
+			}
+		}
+System.out.println(sum);
+	}
+
+}
---
+++ abdul2.java
@@ -0,0 +1,14 @@
+package basicprograms;
+
+public class product_of_two_no {
+
+	public static void main(String[] args) {
+		double product=1d;
+		for (int i = 1; i<=100; i++) 
+		{
+			product=product * i;
+		}
+System.out.println(product);
+	}
+
+}
